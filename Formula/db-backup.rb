310f429bcc73f7806d4ba18eba253150a33ba987
Kuldip Satpute <satputekuldip@gmail.com>
2025-08-28 16:25:01 +0530

    Add DatabaseBackup formula for Homebrew
    This commit introduces the DatabaseBackup formula, a simple tool for database backups. It includes necessary dependencies and installation instructions, ensuring compatibility with Python 3.11. The formula also provides a basic test to verify functionality.

---
+++ Formula/db-backup.rb
@@ -0,0 +1,19 @@
+class DatabaseBackup < Formula
+  include Language::Python::Virtualenv
+
+  desc "A simple database backup tool"
+  homepage "https://github.com/magicstack-llp/db-backup"
+  url "https://github.com/magicstack-llp/db-backup/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "5d594a909086e62ab6a8fb8c6338b7089e0dc682d50e0323d41538cef711171d"
+  license "MIT"
+
+  depends_on "python@3.11"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "#{bin}/db-backup", "--help"
+  end
+end
