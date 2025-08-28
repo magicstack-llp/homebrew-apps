f4b44373e7e39edb8bb2b0ea09bd4925467f0c56
Kuldip Satpute <satputekuldip@gmail.com>
2025-08-28 17:38:02 +0530

    Refactor resource definitions in db-backup formula
    Consolidate resource blocks for dependencies 'click' and 'boto3' in the DbBackup formula. This improves readability and maintains consistency in the resource management within the formula.

--- Formula/db-backup.rb
+++ Formula/db-backup.rb
@@ -11,6 +11,16 @@ class DbBackup < Formula
 
   depends_on "mysql-client"
 
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/source/c/click/click-8.2.1.tar.gz"
+    sha256 "27c491cc05d968d271d5a1db13e3b5a184636d9d930f148c50b038f0d0646202"
+  end
+
+    resource "boto3" do
+    url "https://files.pythonhosted.org/packages/source/b/boto3/boto3-1.40.19.tar.gz"
+    sha256 "772f259fdef6efa752c5744e140c0371593a20a0c728cce91d67b8b58d1090e7"
+  end
+
   def install
     virtualenv_install_with_resources
   end
